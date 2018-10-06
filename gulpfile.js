60e74bda7119110e8d4bae8a0f12c7bde17b2f5d
sanbaimal <kamata.nibaimasu@gmail.com>
2018-10-06 19:37:22 +0900

    [modify] css sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,16 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 // SassとCssの保存先を指定
-gulp.task('sass', function(){
-  gulp.src('./sass/**/*.scss')
-    .pipe(sass({outputStyle: 'expanded'}))
-    .pipe(gulp.dest('./css/'));
+gulp.task('sass', function () {
+  gulp.src('./_src/sass/**/*.scss')
+    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(gulp.dest('./www/css/'));
 });
 
 //自動監視のタスクを作成(sass-watchと名付ける)
-gulp.task('sass-watch', ['sass'], function(){
-  var watcher = gulp.watch('./sass/**/*.scss', ['sass']);
-  watcher.on('change', function(event) {
+gulp.task('sass-watch', ['sass'], function () {
+  var watcher = gulp.watch('./_src/sass/**/*.scss', ['sass']);
+  watcher.on('change', function (event) {
   });
 });
 
